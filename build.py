9449b9799191a978530509af98f5e9033f9087c0
Khaled Hosny <khaledhosny@eglug.org>
2020-03-24 13:37:28 +0200

    Don’t propagate cursive anchors
    Explicitly set them.

--- build.py
+++ build.py
@@ -432,6 +432,8 @@ def propogateAnchors(layer):
             name = anchor.name
             if name.startswith("_") or name in names:
                 continue
+            if name in ("entry", "exit"):
+                continue
             x, y = anchor.position.x, anchor.position.y
             if component.transform != DEFAULT_TRANSFORM:
                 t = Transform(*component.transform.value)
